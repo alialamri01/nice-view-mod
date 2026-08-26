e60715f86c039e8ab4a6654dd92303e018005666
Ali Alamri <54723013+alialamri01@users.noreply.github.com>
2026-08-26 20:53:34 +0200

    Restore peripheral_status.c with original content

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -139,12 +139,12 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
     //lv_obj_center(art);                                         //<--
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);                 //<--
     lv_animimg_set_src(art, (const void **) anim_imgs, 7);      //<--
     lv_animimg_set_duration(art, 3500);                         //<--
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
     lv_animimg_start(art);                                      //<--
     
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
