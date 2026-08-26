9a14bdb09b7834389955f97c5b464233bf1ba232
Ali Alamri <54723013+alialamri01@users.noreply.github.com>
2026-08-26 20:46:36 +0200

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -138,7 +138,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     //lv_img_set_src(art, &stickyCatHeart5);
 
     lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
-    lv_obj_center(art);                                         //<--
+    //lv_obj_center(art);                                         //<--
     lv_animimg_set_src(art, (const void **) anim_imgs, 7);      //<--
     lv_animimg_set_duration(art, 3500);                         //<--
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
