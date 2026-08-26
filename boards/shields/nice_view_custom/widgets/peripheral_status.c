1f8a37575c7689c32c96ab2eb70a9b859beaae89
Ali Alamri <54723013+alialamri01@users.noreply.github.com>
2026-08-26 21:21:08 +0200

    Restore peripheral_status.c with original content

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -143,7 +143,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     //lv_obj_center(art);                                         //<--
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);                 //<--
     lv_animimg_set_src(art, (const void **) anim_imgs, 8);      //<--
-    lv_animimg_set_duration(art, 3500);                         //<--
+    lv_animimg_set_duration(art, 3000);                         //<--
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
     lv_animimg_start(art);                                      //<--
     
